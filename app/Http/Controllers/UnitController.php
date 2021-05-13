a5e814465334c4b935af0c3f258826348783e387
sergiofilho <sergiofilhoja@hotmail.com>
2021-05-13 13:06:31 -0300

    Backend Dev Condominio

---
+++ app/Http/Controllers/UnitController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\Unit;
+use App\Models\UnitPeoples;
+use App\Models\UnitVehicle;
+use App\Models\UnitPet;
+
+class UnitController extends Controller
+{
+    public function getInfo($id)
+    {
+        $array = ['error' => ''];
+
+        $unit = Unit::find($id);
+        if ($unit) {
+            $peoples = UnitPeoples::where('id_unit', $id)->get();
+            $vehicles = UnitVehicle::where('id_unit', $id)->get();
+            $pets = UnitPet::where('id_unit', $id)->get();
+
+            foreach ($peoples as $pKey => $pValue) {
+                $peoples[$pKey]['birthdate'] = date('d/m/Y', strtotime($pValue['birthdate']));
+            }
+
+            $array['peoples'] = $peoples;
+            $array['vehicles'] = $vehicles;
+            $array['pets'] = $pets;
+        } else {
+            $array['error'] = 'Propriedade inexistente';
+        }
+
+        return $array;
+    }
+
+    public function addPerson($id, Request $request)
+    {
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'birthdate' => 'required|date',
+        ]);
+        if (!$validator->fails()) {
+            $name = $request->input('name');
+            $birthdate = $request->input('birthdate');
+
+            $newPerson = new UnitPeoples();
+            $newPerson->id_unit = $id;
+            $newPerson->name = $name;
+            $newPerson->birthdate = $birthdate;
+            $newPerson->save();
+        } else {
+            $array['error'] = $validator->errors()->first();
+            return $array;
+        }
+
+        return $array;
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Unidades
+        DB::table('units')->insert([
+            'name' => 'APT 100',
+            'id_owner' => 1
+        ]);
+        DB::table('units')->insert([
+            'name' => 'APT 101',
+            'id_owner' => 1
+        ]);
+        DB::table('units')->insert([
+            'name' => 'APT 200',
+            'id_owner' => '0'
+        ]);
+        DB::table('units')->insert([
+            'name' => 'APT 201',
+            'id_owner' => '0'
+        ]);
+
+        // Areas
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'Academia',
+            'cover' => 'gym.jpg',
+            'days' => '1,2,4,5',
+            'start_time' => '06:00:00',
+            'end_time' => '22:00:00',
+        ]);
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'Piscina',
+            'cover' => 'pool.jpg',
+            'days' => '1,2,3,4,5',
+            'start_time' => '07:00:00',
+            'end_time' => '23:00:00',
+        ]);
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'Churrasqueira',
+            'cover' => 'barbecue.jpg',
+            'days' => '4,5,6',
+            'start_time' => '09:00:00',
+            'end_time' => '23:00:00',
+        ]);
+
+        // Mural de avisos
+        DB::table('walls')->insert([
+            'title' => 'Título de aviso teste',
+            'body' => 'Lorem Ipsum balsdlasd la nsld aksdh aksjdha ksjdahjskdsa',
+            'datecreated' => '2020-12-20 15:00:00'
+        ]);
+        DB::table('walls')->insert([
+            'title' => 'Alerta geral para todos',
+            'body' => 'Alerta de cuidado para com os moradores, se liguem na covid, ninguém é de ferro',
+            'datecreated' => '2020-12-20 18:00:00'
+        ]);
+    }
+}
